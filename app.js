5fb3821054fe783d00a463d9445476390882a129
Dario <dario.mijic@gmx.com>
2019-07-28 22:10:14 +0200

    Added edit and delete routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -26,6 +27,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Method override middleware
+app.use(methodOverride('_method'))
+
 // Index route
 app.get('/', (req, res) => {
     const title = 'Welcome';
@@ -39,16 +43,86 @@ app.get('/about', (req, res) => {
     res.render('about')
 });
 
+// Idea index page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas =>{
+    res.render('ideas/index', {
+        ideas: ideas
+        });
+    });
+});
+
 // Add Idea route
 app.get('/ideas/add', (req,res) => {
     res.render('ideas/add')
 });
 
+// Edit Idea route
+app.get('/ideas/edit/:id', (req,res) => {
+    Idea.findOne({
+        _id:req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit', {
+            idea:idea
+        })
+    })
+});
+
 // Process form
 app.post('/ideas', (req, res) => {
-    
-    res.send('ok');
+    let errors = [];
+    if(!req.body.title){
+        errors.push({text:'Please add a title'});
+    }
+    if(!req.body.details){
+        errors.push({text: 'Please add some details'})
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+        errors: errors,
+        title: req.body.title,
+        details: req.body.details
+    });
+} else {
+    const newUser = {
+        title: req.body.title,
+        details: req.body.details
+    }
+    new Idea(newUser)
+    .save()
+    .then(idea => {
+        res.redirect('/ideas');
+    })
+}
 });
+
+// Edit form process
+app.put('/ideas/:id', (req, res) =>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        // new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+        idea.save()
+        .then( idea =>{
+            res.redirect('/ideas')
+        })
+    })
+})
+
+//Delete Idea
+app.delete('/ideas/:id', (req, res) =>{
+    Idea.remove({_id: req.params.id})
+    .then(() => {
+        res.redirect('/ideas');
+    })
+})
 
 const port = 5000;
 
