62798e8162ac914cffddb88a0f3620af6dadabba
Dario <dario.mijic@gmx.com>
2019-08-30 06:25:13 +0200

    Login with passport and bcrypt

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
+const path = require('path');
+const passport = require('passport');
 
 const app = express();
 
 // Load routes
 const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport config
+require('./config/passport')(passport);
 
 // Map global promise
 mongoose.Promise = global.Promise;
@@ -32,6 +38,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method override middleware
 app.use(methodOverride('_method'));
 
@@ -42,6 +51,10 @@ app.use(session({
     saveUninitialized: true,
   }));
 
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   app.use(flash());
 
 // GLobal variables
@@ -49,6 +62,7 @@ app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
@@ -66,19 +80,9 @@ app.get('/about', (req, res) => {
 });
 
 
-
-// User login route
-app.get('/users/login', (req, res) => {
-    res.render('login')
-});
-
-// User register route
-app.get('/users/register', (req, res) => {
-    res.render('register')
-});
-
 // Use routes
 app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
